a3049d7f03c635c6b5c524fb657580dc33e60ba5
Philipp <p.omelzenko@outlook.de>
2024-11-01 00:56:21 +0100

    Projektdateien hinzufügen.

---
+++ Kilometer Rechner/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Kilometer_Rechner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Kilometer Rechner/Models/City.cs
@@ -0,0 +1,10 @@
+﻿namespace Kilometer_Rechner.Models
+{
+    internal class City
+    {
+        public string PLZ { get; set; }
+        public string Ort { get; set; }
+        public string Lon { get; set; }
+        public string Lat {  get; set; }
+    }
+}
